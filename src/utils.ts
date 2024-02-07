7c4a0b1b16c00bad2f1101619531b285554be038
Geoff Gauchet <geoff@zhephree.com>
2024-02-06 22:34:55 -0600

    add utils

---
+++ src/utils.ts
@@ -0,0 +1,62 @@
+import axios, {isCancel, AxiosError} from 'axios';
+import { Logger } from 'homebridge';
+
+
+export class wledAPI {
+  private baseUrl: string;
+
+  constructor(public readonly host: string, public readonly log: Logger){
+    this.host = host;
+    this.baseUrl = `http://${this.host}/json`;
+    this.log = log;
+  }
+
+  async get(method: string){
+    try {
+      const response = await axios.get(`${this.baseUrl}/${method}`);
+      return response.data;
+    }catch(err: unknown){
+      this.log.error('Could not make API request to method:', method);
+      let error;
+      if(typeof err === 'string'){
+        error = err;
+      }else if(err instanceof Error){
+        error = err.message;
+      }
+      this.log.error(error);
+    }
+  }
+
+  async post(method: string, data: object = {}){
+    try {
+      const response = await axios.post(`${this.baseUrl}/${method}`, data);
+      return response.data;
+    }catch(err: unknown){
+      this.log.error('Could not make API request to method:', method);
+      let error;
+      if(typeof err === 'string'){
+        error = err;
+      }else if(err instanceof Error){
+        error = err.message;
+      }
+      this.log.error(error);
+
+    }
+  }
+
+  async state(){
+    return await this.get('state');
+  }
+
+  async info(){
+    return await this.get('info');
+  }
+
+  async effects(){
+    return await this.get('effects');
+  }
+
+  async palettes(){
+    return await this.get('palettes');
+  }
+}
